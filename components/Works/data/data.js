f5773dedbc7ee8b653afd1f3c905918d2fa92800
Aylin Morales <aylinmorales@Aylins-MacBook-Pro.local>
2023-08-28 21:57:23 -0700

    files added

---
+++ components/Works/data/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        image_url: "https://user-images.githubusercontent.com/117704967/224256864-5450c5bf-b275-401e-88cd-7cfd7f3643e2.png",
+        work_title: "Weather Dashboard",
+        url: "https://byaylin.github.io/weather-dashboard/",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Design & Dev"
+    },
+    {
+        id: 2,
+        image_url: "https://user-images.githubusercontent.com/117704967/263124238-233a3bfb-19a2-4227-be51-3c1db5d32e00.jpg",
+        work_title: "Employee Tracker",
+        url: "https://github.com/byaylin/employee-tracker",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Command-line app"
+    },
+    {
+        id: 3,
+        image_url: "https://user-images.githubusercontent.com/117704967/209288837-4e566321-4e53-45c0-af2f-58e0c758c317.png",
+        work_title: "Password Generator",
+        url: "https://github.com/byaylin/password-generator",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Dev"
+    },
+];
+
+export default data;
